f89e81808c82225eba03493561887a964f9571d9
Zen Dignan <zen@dignan.dev>
2025-11-04 21:34:03 -0500

    Update deps, Handle loading better, serve Bootstrap in bundle, add Source button, colors, also cleanup

--- src/components/SectionEntry.tsx
+++ src/components/SectionEntry.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { CourseSection } from '../models/Schedule';
-import { Card, CardBody, CardText, CardTitle } from 'reactstrap';
+import { Card, CardBody, CardTitle } from 'reactstrap';
 import { capacityToString } from '../utils';
 import { Time } from '.';
 
@@ -8,14 +8,21 @@ export const SectionEntry: FC<{ readonly section: CourseSection }> = ({ section 
   return (
     <Card>
       <CardBody>
-        <CardTitle tag="p"> {section.term} - {section.instructors} </CardTitle>
+        <CardTitle tag="p">
+          {section.term} - {section.instructors}
+        </CardTitle>
         <ul>
           <li>Enrollment: {capacityToString(section.enrollment)} seats remaining</li>
           <li>Waitlist: {capacityToString(section.waitlist)} seats remaining</li>
         </ul>
         <p>Patterns</p>
-        {section.patterns.map((p) => <>{p.days.map((d) => `${d} `)}<Time time={p.startTime} /> - <Time time={p.endTime} /> @ {p.locationId}</>)}
+        {section.patterns.map((p) => (
+          <>
+            {p.days.map((d) => `${d} `)}
+            <Time time={p.startTime} /> - <Time time={p.endTime} /> @ {p.locationId}
+          </>
+        ))}
       </CardBody>
     </Card>
-  )
+  );
 };
